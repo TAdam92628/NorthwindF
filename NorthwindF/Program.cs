690b57c528b748e520c251e1f40215ceaf1d9e77
Ádám Turcsik <turcsik.adam@int.dss.hu>
2023-09-20 20:49:19 +0200

    Add project files.

---
+++ NorthwindF/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
